904bcc6a8921f54f517d59e2658f3df7e15e3a62
raindropcity <raindropcity0209@gmail.com>
2023-06-12 00:49:00 +0800

    submit Week-3 Assignment-1

---
+++ Week-3/Assignment-1/Assignment1/Assignment1/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Assignment1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Assignment1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
