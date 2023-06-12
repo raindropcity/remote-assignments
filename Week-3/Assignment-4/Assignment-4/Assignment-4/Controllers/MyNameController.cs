96f676b7baf887cf12b960d0d1d5ce91e83835c6
raindropcity <raindropcity0209@gmail.com>
2023-06-13 00:32:22 +0800

    submit Week-3 Assignment-4 (need some help) (didn't totally match the assignment requirement)

---
+++ Week-3/Assignment-4/Assignment-4/Assignment-4/Controllers/MyNameController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment_4.Controllers
+{
+    public class MyNameController : Controller
+    {
+        [HttpGet("{myName}")]
+        public ActionResult Index(string myName)
+        {
+            string name = Request.Cookies["name"];
+
+            if (!string.IsNullOrEmpty(myName))
+            {
+                ViewBag.Name = name;
+            }
+
+            return View();
+        }
+
+        public ActionResult SetName(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                var cookieOptions = new CookieOptions();
+                cookieOptions.Expires = DateTime.Now.AddDays(7);
+                cookieOptions.Path = "/";
+                Response.Cookies.Append("name", name, cookieOptions);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
