afbd0b311f3d199cb7d957404054221adc211f92
raindropcity <raindropcity0209@gmail.com>
2023-06-13 22:12:58 +0800

    updated Assignment-3(still need help)

---
+++ Week-3/Assignment-3/Assignment-2/Assignment-2/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Assignment_2.Controllers
+{
+    [Route("data")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("{number}")]
+        public IActionResult GetData(string? number)
+        {
+            if (number == null)
+            {
+                return BadRequest("Lack of Parameter");
+            }
+            else if (!int.TryParse(number, out int value))
+            {
+                return BadRequest("Wrong Parameter");
+            }
+            else
+            {
+                int sum = 0;
+                for (int i = 1; i <= value; i++)
+                {
+                    sum += i;
+                }
+                return Ok(sum);
+            }
+        }
+    }
+}
