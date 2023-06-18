d766f7e0654525c1a797c555920c4c0d046b3d8a
raindropcity <raindropcity0209@gmail.com>
2023-06-19 00:44:32 +0800

    submit Week-4 Assignment-1 (need some help)

---
+++ Week-4/Assignment-1/Assignment-1/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using Assignment_1.Data;
+using Assignment_1.Models;
+using Assignment_1.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment_1.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly AssignmentDbContext assignmentDbContext;
+        public UsersController(AssignmentDbContext assignmentDbContext)
+        {
+            this.assignmentDbContext = assignmentDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddUserViewModel addUserRequest)
+        {
+            var user = new User()
+            {
+                Id = Guid.NewGuid(),
+                Email = addUserRequest.Email,
+                Password = addUserRequest.Password
+            };
+
+            await assignmentDbContext.Users.AddAsync(user);
+            await assignmentDbContext.SaveChangesAsync();
+            return RedirectToAction("Add");
+            // 記得改成redirect到"member page" 寫welcome
+        }
+    }
+}
---
+++ Week-4/Assignment-1/Assignment-1/Models/Domain/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Assignment_1.Models.Domain
+{
+    // we want to create a model which we will be able to use as a domain model or entity
+    // we want to create "users" and perform CRUD operation on "users", so we will create a "users" model
+    public class User
+    {
+        // "Guid" represents a globally unique identifier
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        /* we have our properties defined for the User domain model and this is what we want to persist in the database and then further retrieve the list of users and a single user */
+        // we are going to use this domain model inside our "AssignmentDbContext.cs"
+    }
+}
