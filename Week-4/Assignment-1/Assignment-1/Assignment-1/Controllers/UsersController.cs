a71e024ce1ef6d7a6c00e3d4cdfef09af07ceb73
raindropcity <raindropcity0209@gmail.com>
2023-06-19 17:26:54 +0800

    submit Week-4 Assignment-1

---
+++ Week-4/Assignment-1/Assignment-1/Assignment-1/Controllers/UsersController.cs
@@ -0,0 +1,73 @@
+﻿using Assignment_1.Data;
+using Assignment_1.Models;
+using Assignment_1.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment_1.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly AssignmentDbContext assignmentDbContext;
+        public UsersController(AssignmentDbContext assignmentDbContext)
+        {
+            this.assignmentDbContext = assignmentDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Member()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(AddUserViewModel addUserRequest)
+        {
+            var user = new User()
+            {
+                Id = Guid.NewGuid(),
+                Email = addUserRequest.Email,
+                Password = addUserRequest.Password
+            };
+
+            if (!assignmentDbContext.Users.Any(e => e.Email == user.Email))
+            {
+                await assignmentDbContext.Users.AddAsync(user);
+                await assignmentDbContext.SaveChangesAsync();
+                return RedirectToAction("Member");
+            }
+
+            return Content("The email address has been registered");
+        }
+
+        [HttpPost]
+        public IActionResult SignIn(User signInRequest)
+        {
+            var user = new User()
+            {
+                Id = Guid.NewGuid(),
+                Email = signInRequest.Email,
+                Password = signInRequest.Password
+            };
+
+            if (assignmentDbContext.Users.Any(e => e.Email == user.Email))
+            {
+                return RedirectToAction("Member");
+            }
+
+            return Content("Please sign up first");
+        }
+    }
+}
---
+++ Week-4/Assignment-1/Assignment-1/Assignment-1/Views/Users/Member.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Assignment_1.Views.Users
+{
+    public class MemberModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
